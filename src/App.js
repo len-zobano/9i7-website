d9eb733321b812f4c0eb419caea35e67b539afd5
len.zobano <ben@bens-mbp-2.lan>
2022-11-30 18:56:21 -0600

    basic drawing of yellow circle on window resize

--- src/App.js
+++ src/App.js
@@ -15,6 +15,33 @@ function useWindowDimensions() {
 
   useEffect(() => {
     function handleResize() {
+
+
+      function drawCircle(ctx, x, y, radius, fill, stroke, strokeWidth) {
+        ctx.beginPath()
+        ctx.arc(x, y, radius, 0, 2 * Math.PI, false)
+        if (fill) {
+          ctx.fillStyle = fill
+          ctx.fill()
+        }
+        if (stroke) {
+          ctx.lineWidth = strokeWidth
+          ctx.strokeStyle = stroke
+          ctx.stroke()
+        }
+      }
+
+      console.log("dimensions: ",getWindowDimensions());
+
+      let dimensions = getWindowDimensions();
+
+      var canvas = document.getElementById("test-canvas");
+      var ctx = canvas.getContext("2d");
+      ctx.fillStyle = "black";
+      ctx.fillRect(0, 0, dimensions.width, dimensions.height);
+
+      drawCircle(ctx, dimensions.width/2, dimensions.height/2, 100, "#ffff00","black",0);
+
       setWindowDimensions(getWindowDimensions());
     }
 
@@ -28,7 +55,7 @@ function useWindowDimensions() {
 
 function Canvas(props) {
   const { height, width } = useWindowDimensions();
-  return <canvas width={width} height={height}></canvas>;
+  return <canvas id="test-canvas" width={width} height={height}></canvas>;
 }
 
 function App() {
@@ -36,7 +63,7 @@ function App() {
   const { height, width } = useWindowDimensions();
 
   return (
-    <Canvas name="Len"/>
+    <Canvas/>
   );
 }
 
