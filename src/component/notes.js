938fef04fe7eb7b4c6b70009153cfba3485fd25f
len.zobano <ben@bens-mbp-2.lan>
2023-01-13 09:48:58 -0600

    rendering hello world note

--- src/component/notes.js
+++ src/component/notes.js
@@ -1,8 +1,30 @@
+
+class NoteNode {
+    #id = ''; //unique id string
+    #children = [];
+    #text = '';
+    #encryption = ''; //if not a blank string, is an encryption key ID
+    #metadata = {}; //a JSON object with implementation-specific contents
+    #complete = -1; //a negative value means it isn
+    #dueDate = null;
+
+    #categories = []; //must maintain parity with category -> node relationship
+
+    render () {
+        return <div class="noteNode"> {this.#text} </div>
+    }
+
+    constructor (text) {
+        this.#text = text;
+    }
+}
+
 function Notes() {
+
+    let rootNode = new NoteNode('Hello world note');
+
   return (
-    <div class="note">
-        Hello world
-    </div>
+    rootNode.render()
   );
 }
 
