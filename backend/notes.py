7f0b8098ea7c861995a5e9b213f7a0f464b12141
len.zobano <ben@bens-mbp-2.lan>
2023-02-16 16:01:53 -0600

    hello world json response

--- backend/notes.py
+++ backend/notes.py
@@ -1,13 +1,21 @@
 import http.server
 import socketserver
 from http import HTTPStatus
+import json
 
+responseObject = {
+    "id": "1",
+    "version": 1,
+    "metadata": {
+        "text": "hello world"
+    }
+} 
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        self.wfile.write(b'')
+        self.wfile.write(bytes( json.dumps(responseObject), 'utf-8'))
 
 
 httpd = socketserver.TCPServer(('', 8000), Handler)
