590718939b2c26394f68e932aa8dcd6acfab118e
len.zobano <ben@bens-mbp-2.lan>
2024-04-17 21:07:00 -0500

    cards looking refined

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
             <NavigationCard imageSource="particle-screenshot.png" to="/particle" title="Particle Simulator" caption="A simple environment simulating particle physics inefficiently because it looks cool">
 
             </NavigationCard>
-            <NavigationCard imageSource="note-icon.png" to="/notes/0" title="Notes" caption="A tree-structured note-taking app for better-organized data">
+            <NavigationCard imageSource="note-icon.png" to="/notes/0" title="Notes" caption="A tree-structured note-taking app for better-organized data. I'm putting a lot of text here so I can test how the cards look at different heights with varying caption lengths. Look at me, look at me, wow.">
 
             </NavigationCard>
           </div>
