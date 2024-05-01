51bfd33f0ccd4dd668e45e80f5ed4258dadcf7b5
Leonard Zobano <len.zobano@gmail.com>
2024-04-30 23:18:07 -0500

    basic mobile responsiveness on home page, but doesn't look as cool because the heights don't vary with text

--- src/routes/root.js
+++ src/routes/root.js
@@ -3,8 +3,6 @@ import Home from "../component/home.js";
 export default function Root() {
     return (
       <>
-        Hello world from root.js
-        Home: 
         <Home />
       </>
     );
