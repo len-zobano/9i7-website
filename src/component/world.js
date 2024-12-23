c5975429790d49ff966bc081c4e068d1aba728c3
Leonard Zobano <len.zobano@gmail.com>
2024-12-23 13:22:55 -0600

    3 deep cube stable in zero gravity

--- src/component/world.js
+++ src/component/world.js
@@ -162,7 +162,7 @@ class World {
   #projectionMatrix = null;
   #gridSystem = null;
   #gl = null;
-  #globalGravityVector = glMatrix.vec3.fromValues(0,-50,0);
+  #globalGravityVector = glMatrix.vec3.fromValues(0,0,0);
   #isRunning = false;
 
   get maxRepulsionMagnitude () {
